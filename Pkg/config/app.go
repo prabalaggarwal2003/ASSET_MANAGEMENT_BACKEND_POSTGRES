933df109dcf7a8adf4004479db4b85230bd7a4ed
Aryan Pillai <aryanpillai518@gmail.com>
2024-09-23 14:49:34 +0530

    first commit

---
+++ Pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+
+	d, err := gorm.Open("postgres", "host=localhost port=5432 dbname=issue user=postgres password=<your_db_password> connect_timeout=10 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
